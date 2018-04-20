f7e9f0ed0682a8ae128fa43671401f03d6bbee53
uygar <uygaruyaniksoy@gmail.com>
2018-04-20 23:08:24 +0300

    update pp

--- public/profile.js
+++ public/profile.js
@@ -7,12 +7,34 @@ class Profile extends React.Component {
     this.state = {
       user: {},
     };
+
+    this.setPP = this.setPP.bind(this);
+    this.fetchSelf = this.fetchSelf.bind(this);
+    this.uploadPicture = this.uploadPicture.bind(this);
   }
 
   componentWillMount() {
+    this.fetchSelf();
+  }
+
+  fetchSelf() {
     client.login().then(() => {
       db.collection('users').find({id: parseInt(localStorage.getItem('facebook-clone-id'))}).limit(100).execute().then((a) => this.setState({ user: a[0] }));
     });
+  }
+
+  setPP(p) {
+    db.collection('users').updateOne(
+      {id: this.state.user.id},
+      {"$set": {image: p, pictures: this.state.user.pictures.filter(a => a !== p).concat(this.state.user.image)}}
+    ).then(this.fetchSelf);
+  }
+
+  uploadPicture() {
+    db.collection('users').updateOne(
+      {id: this.state.user.id},
+      {"$set": {pictures: this.state.user.pictures.concat(this.state.imageLink)}}
+    ).then(this.fetchSelf);
   }
 
   render() {
@@ -41,18 +63,43 @@ class Profile extends React.Component {
               </div>
               <div
                 style={{
-                  height: 200,
+                  minHeight: 200,
                 }}
               >
                 <h2>
-                  all images
+                  Pictures
                 </h2>
-                <h4>
-                  add photo
-                </h4>
-                <h4>
-                  set pp
-                </h4>
+                <div className="row">
+                  <div className="col-6">
+                    <img className="card" src={this.state.user.image} style={{ width: 'inherit', }} />
+                  </div>
+                  {
+                    this.state.user.pictures && this.state.user.pictures.map(p => (
+                      <div className="col-6" key={p}>
+                        <img className="card" src={p} style={{ width: 'inherit', }} onMouseOver={(a) => this.setState({ hover: p })}/>
+                        {
+                          this.state.hover === p && <button className="btn btn-success" onClick={() => this.setPP(p)}>set pp</button>
+                        }
+                      </div>
+                    ))
+                  }
+                </div>
+                <br />
+                <button
+                  className="col-12 btn btn-success"
+                  onClick={() => this.setState(
+                    {
+                      modal: {
+                        callback: () => this.uploadPicture(),
+                        heading: 'Upload Picture',
+                        text: 'Please add the link of the picture you want to upload',
+                      },
+                    },
+                    () => $('#myModal').modal('show')
+                  )}
+                >
+                  add picture
+                </button>
               </div>
               <div
                 style={{
@@ -83,6 +130,37 @@ class Profile extends React.Component {
             </div>
           </div>
         </div>
+        {
+          this.state.modal &&
+          <div className="modal fade" id="myModal">
+            <div className="modal-dialog">
+              <div className="modal-content">
+
+                <div className="modal-header">
+                  <h4 className="modal-title">{this.state.modal.heading}</h4>
+                  <button type="button" className="close" data-dismiss="modal">X</button>
+                </div>
+
+                <div className="modal-body">
+                  {this.state.modal.text}
+                </div>
+
+                <input
+                  type="text"
+                  className="form-control col-10 offset-1"
+                  onChange={e => this.setState({ imageLink: e.nativeEvent.target.value })}
+                />
+                <br />
+
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-success" data-dismiss="modal" onClick={this.state.modal.callback}>Upload</button>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        }
       </div>
     )
   }
