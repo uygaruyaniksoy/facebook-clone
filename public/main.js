67c979db92dac31f15a4d315380d37ded142a9ad
uygar <uygaruyaniksoy@gmail.com>
2018-04-21 03:13:30 +0300

    add friend, add picture, list friends, add friends

--- public/main.js
+++ public/main.js
@@ -1,51 +1,60 @@
 const initUsers = () => {
   // these pictures are scraped from https://www.pexels.com/search/animal/
   const pictures = [
-    "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/9291/nature-bird-flying-red.jpg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/45164/mare-animal-nature-ride-45164.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/45911/peacock-plumage-bird-peafowl-45911.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/38278/tiger-head-wildlife-animal-38278.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/45242/wolf-torque-wolf-moon-cloud-45242.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/460823/pexels-photo-460823.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/162140/duckling-birds-yellow-fluffy-162140.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/34231/antler-antler-carrier-fallow-deer-hirsch.jpg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/50582/selfie-monkey-self-portrait-macaca-nigra-50582.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/70080/elephant-africa-african-elephant-kenya-70080.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/225869/pexels-photo-225869.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/326900/pexels-photo-326900.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/302304/pexels-photo-302304.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/160933/girl-rabbit-friendship-love-160933.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/39571/gorilla-silverback-animal-silvery-grey-39571.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/97533/pexels-photo-97533.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/39857/leopard-leopard-spots-animal-wild-39857.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/39504/giraffe-animal-funny-facial-expression-39504.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/434090/pexels-photo-434090.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/234054/pexels-photo-234054.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/86462/red-kite-bird-of-prey-milan-raptor-86462.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/349758/hummingbird-bird-birds-349758.jpeg?auto=compress&cs=tinysrgb&h=350",
-    "https://images.pexels.com/photos/236636/pexels-photo-236636.jpeg?auto=compress&cs=tinysrgb&h=350",
+    {likes: 0, url:"https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/9291/nature-bird-flying-red.jpg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/45164/mare-animal-nature-ride-45164.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/45911/peacock-plumage-bird-peafowl-45911.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/38278/tiger-head-wildlife-animal-38278.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/45242/wolf-torque-wolf-moon-cloud-45242.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/460823/pexels-photo-460823.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/162140/duckling-birds-yellow-fluffy-162140.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/34231/antler-antler-carrier-fallow-deer-hirsch.jpg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/50582/selfie-monkey-self-portrait-macaca-nigra-50582.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/70080/elephant-africa-african-elephant-kenya-70080.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/225869/pexels-photo-225869.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/326900/pexels-photo-326900.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/302304/pexels-photo-302304.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/160933/girl-rabbit-friendship-love-160933.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/39571/gorilla-silverback-animal-silvery-grey-39571.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/33045/lion-wild-africa-african.jpg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/97533/pexels-photo-97533.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/39857/leopard-leopard-spots-animal-wild-39857.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/39504/giraffe-animal-funny-facial-expression-39504.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/434090/pexels-photo-434090.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/234054/pexels-photo-234054.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/86462/red-kite-bird-of-prey-milan-raptor-86462.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/349758/hummingbird-bird-birds-349758.jpeg?auto=compress&cs=tinysrgb&h=350"},
+    {likes: 0, url:"https://images.pexels.com/photos/236636/pexels-photo-236636.jpeg?auto=compress&cs=tinysrgb&h=350"},
   ];
 
   let i = 0;
-  [
-    {id: Math.random()*100000 | 0, name: 'Preston', surname: 'Laver', image: 'https://orig00.deviantart.net/23a2/f/2010/352/f/7/headshotid_by_bokogreat_stock-d355xf3.jpg', friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
-    {id: Math.random()*100000 | 0, name: 'Delilah', surname: 'Nowlin', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHlQOv9gOKhTWjZEMPYUB15eAmcWEjQE9WStjuDX19cM5ZoZf', friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
-    {id: Math.random()*100000 | 0, name: 'Casandra', surname: 'Mckell', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV31p-oGNXwsOpNRAKcMq4HDPER9asurPxP_T4BjA6s1snffd-aw', friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
-    {id: Math.random()*100000 | 0, name: 'Patty', surname: 'Tejeda', image: 'https://media.istockphoto.com/photos/asian-women-profile-picture-id139662784?k=6&m=139662784&s=612x612&w=0&h=GhwIW_Q3TIhdFgrVlbg7sMtLXzRNmaiLzKtjIQhX_sE=', friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
-    {id: Math.random()*100000 | 0, name: 'Evelia', surname: 'Peterka', image: 'https://s3.eu-central-1.amazonaws.com/artistarea.gallereplay.com/production/user_9/picture_2405201614728.jpg', friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
-    {id: Math.random()*100000 | 0, name: 'Sona', surname: 'Girardi', image: 'https://orig00.deviantart.net/b682/f/2013/135/4/3/profile_picture_by_mellodydoll_stock-d65fbf8.jpg', friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
-    {id: Math.random()*100000 | 0, name: 'Ashton', surname: 'Every', image: 'http://actionsportsgames.com.au/cms/wp-content/uploads/2014/11/profile_picture_by_naivety_stock-d5x8lbn.jpg', friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
-    {id: Math.random()*100000 | 0, name: 'Jessi', surname: 'Swing', image: 'https://images.pexels.com/photos/736715/pexels-photo-736715.jpeg?auto=compress&cs=tinysrgb&h=350', friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
-    {id: Math.random()*100000 | 0, name: 'Keneth', surname: 'Eder', image: 'https://img00.deviantart.net/038e/i/2010/150/b/0/me_2010_by_axy_stock.jpg', friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
-    {id: Math.random()*100000 | 0, name: 'Nannie', surname: 'Petrick', image: 'https://orig00.deviantart.net/b893/f/2011/227/f/6/profile_picture_by_aehireiel_stock-d46o5ls.jpg', friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i], ], }
-  ].map(a => db.collection('users').insertOne(a));
+  let users = [
+    {id: Math.random()*100000 | 0, name: 'Preston', surname: 'Laver', image: {likes: 0, url: 'https://orig00.deviantart.net/23a2/f/2010/352/f/7/headshotid_by_bokogreat_stock-d355xf3.jpg'}, friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
+    {id: Math.random()*100000 | 0, name: 'Delilah', surname: 'Nowlin', image: {likes: 0, url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHlQOv9gOKhTWjZEMPYUB15eAmcWEjQE9WStjuDX19cM5ZoZf'}, friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
+    {id: Math.random()*100000 | 0, name: 'Casandra', surname: 'Mckell', image: {likes: 0, url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV31p-oGNXwsOpNRAKcMq4HDPER9asurPxP_T4BjA6s1snffd-aw'}, friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
+    {id: Math.random()*100000 | 0, name: 'Patty', surname: 'Tejeda', image: {likes: 0, url: 'https://media.istockphoto.com/photos/asian-women-profile-picture-id139662784?k=6&m=139662784&s=612x612&w=0&h=GhwIW_Q3TIhdFgrVlbg7sMtLXzRNmaiLzKtjIQhX_sE='}, friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
+    {id: Math.random()*100000 | 0, name: 'Evelia', surname: 'Peterka', image: {likes: 0, url: 'https://s3.eu-central-1.amazonaws.com/artistarea.gallereplay.com/production/user_9/picture_2405201614728.jpg'}, friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
+    {id: Math.random()*100000 | 0, name: 'Sona', surname: 'Girardi', image: {likes: 0, url: 'https://orig00.deviantart.net/b682/f/2013/135/4/3/profile_picture_by_mellodydoll_stock-d65fbf8.jpg'}, friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
+    {id: Math.random()*100000 | 0, name: 'Ashton', surname: 'Every', image: {likes: 0, url: 'http://actionsportsgames.com.au/cms/wp-content/uploads/2014/11/profile_picture_by_naivety_stock-d5x8lbn.jpg'}, friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
+    {id: Math.random()*100000 | 0, name: 'Jessi', surname: 'Swing', image: {likes: 0, url: 'https://images.pexels.com/photos/736715/pexels-photo-736715.jpeg?auto=compress&cs=tinysrgb&h=350'}, friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
+    {id: Math.random()*100000 | 0, name: 'Keneth', surname: 'Eder', image: {likes: 0, url: 'https://img00.deviantart.net/038e/i/2010/150/b/0/me_2010_by_axy_stock.jpg'}, friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i++], ], },
+    {id: Math.random()*100000 | 0, name: 'Nannie', surname: 'Petrick', image: {likes: 0, url: 'https://orig00.deviantart.net/b893/f/2011/227/f/6/profile_picture_by_aehireiel_stock-d46o5ls.jpg'}, friends: [], pictures: [ pictures[i++], pictures[i++], pictures[i], ], }
+  ];
+  Array(40).fill().forEach(() => { // 40 friendships
+    const f = Math.random()*10 | 0;
+    const s = Math.random()*10 | 0;
+    if (users[f].friends.indexOf(users[s].id) < 0 && f !== s) {
+      users[f].friends.push(users[s].id);
+      users[s].friends.push(users[f].id);
+    }
+  });
+  users.forEach(a => db.collection('users').insertOne(a));
 };
 
 const delUsers = () => {
@@ -182,7 +191,7 @@ class MainPage extends React.Component {
             this.state.users.map(u => (
               <a key={u.id} onClick={() => db.collection('users').deleteOne({ id: u.id }).then(this.fetchUsers)}>
                 <div className="row" style={{ cursor: 'pointer', margin: 5 }}>
-                  <img src={u.image} style={{ width: 50, height: 50 }}/>
+                  <img src={u.image.url} style={{ width: 50, height: 50 }}/>
                   <h4>
                     {`${u.name} ${u.surname}`}
                   </h4>
@@ -214,7 +223,7 @@ class MainPage extends React.Component {
                 }}
               >
                 <div className="row" style={{ cursor: 'pointer', margin: 5 }}>
-                  <img src={u.image} style={{ width: 50, height: 50 }}/>
+                  <img src={u.image.url} style={{ width: 50, height: 50 }}/>
                   <h4>
                     {`${u.name} ${u.surname}`}
                   </h4>
--- public/profile.js
+++ public/profile.js
@@ -5,21 +5,51 @@ class Profile extends React.Component {
     super();
 
     this.state = {
-      user: {},
+      user: {
+        image: {},
+        friends: [],
+      },
     };
 
     this.setPP = this.setPP.bind(this);
     this.fetchSelf = this.fetchSelf.bind(this);
+    this.fetchUsers = this.fetchUsers.bind(this);
     this.uploadPicture = this.uploadPicture.bind(this);
+    this.addFriend = this.addFriend.bind(this);
   }
 
   componentWillMount() {
     this.fetchSelf();
+    this.fetchUsers();
+  }
+
+  fetchUsers() {
+    client.login().then(() => {
+      db.collection('users').find().limit(100).execute().then((users) => this.setState(
+        { users },
+      ));
+    });
   }
 
   fetchSelf() {
     client.login().then(() => {
-      db.collection('users').find({id: parseInt(localStorage.getItem('facebook-clone-id'))}).limit(100).execute().then((a) => this.setState({ user: a[0] }));
+      db.collection('users')
+        .find({id: parseInt(localStorage.getItem('facebook-clone-id'))})
+        .limit(100)
+        .execute()
+        .then((a) => a[0] && this.setState(
+          { user: a[0] },
+          () => this.state.user.friends.forEach((f, i) => db.collection('users')
+            .findOne({id: f})
+            .then(friend => this.setState({
+              user: Object.assign(
+                {},
+                this.state.user,
+                { friends: [...this.state.user.friends.slice(0, i), friend, ...this.state.user.friends.slice(i + 1)] }
+              ),
+            }))
+          )
+        ));
     });
   }
 
@@ -34,6 +64,13 @@ class Profile extends React.Component {
     db.collection('users').updateOne(
       {id: this.state.user.id},
       {"$set": {pictures: this.state.user.pictures.concat(this.state.imageLink)}}
+    ).then(this.fetchSelf);
+  }
+
+  addFriend() {
+    db.collection('users').updateOne(
+      {id: this.state.user.id},
+      {"$set": {friends: this.state.user.friends.map(u => u.id).concat(this.state.friendId)}}
     ).then(this.fetchSelf);
   }
 
@@ -59,7 +96,7 @@ class Profile extends React.Component {
                 <h2>
                   {`${this.state.user.name} ${this.state.user.surname}`}
                 </h2>
-                <img src={this.state.user.image} style={{ width: 150, height: 150 }}/>
+                <img src={this.state.user.image.url} style={{ width: 150, height: 150 }}/>
               </div>
               <div
                 style={{
@@ -71,14 +108,30 @@ class Profile extends React.Component {
                 </h2>
                 <div className="row">
                   <div className="col-6">
-                    <img className="card" src={this.state.user.image} style={{ width: 'inherit', }} />
+                    <img className="card" src={this.state.user.image.url} style={{ width: 'inherit', }} />
+                    <div className="alert alert-success col-12" style={{ padding: '0px 20px' }}>
+                      {
+                        `${this.state.user.image.likes} like(s)`
+                      }
+                      <button className="btn btn-success col-6">
+                        Like
+                      </button>
+                    </div>
                   </div>
                   {
                     this.state.user.pictures && this.state.user.pictures.map(p => (
-                      <div className="col-6" key={p}>
-                        <img className="card" src={p} style={{ width: 'inherit', }} onMouseOver={(a) => this.setState({ hover: p })}/>
+                      <div className="col-6" key={p.url}>
+                        <img className="card" src={p.url} style={{ width: 'inherit', }} onMouseOver={(a) => this.setState({ hover: p })}/>
+                        <div className="alert alert-success col-12" style={{ padding: '0px 20px' }}>
+                          {
+                            `${p.likes} like(s)`
+                          }
+                          <button className="btn btn-success col-6">
+                            Like
+                          </button>
+                        </div>
                         {
-                          this.state.hover === p && <button className="btn btn-success" onClick={() => this.setPP(p)}>set pp</button>
+                          this.state.hover === p && <button style={{ marginTop: '-1rem' }} className="btn btn-success" onClick={() => this.setPP(p)}>set pp</button>
                         }
                       </div>
                     ))
@@ -93,6 +146,8 @@ class Profile extends React.Component {
                         callback: () => this.uploadPicture(),
                         heading: 'Upload Picture',
                         text: 'Please add the link of the picture you want to upload',
+                        inputBox: true,
+                        action: 'Upload',
                       },
                     },
                     () => $('#myModal').modal('show')
@@ -101,17 +156,46 @@ class Profile extends React.Component {
                   add picture
                 </button>
               </div>
+              <br />
               <div
                 style={{
                   height: 200,
                 }}
               >
-                <h2>
-                  friends
-                </h2>
-                <h4>
-                  add friends
-                </h4>
+                <div className="row">
+                  <h2 className="col-4">
+                    Friends
+                  </h2>
+                  <h2 className="col-4">
+                  </h2>
+                  <button
+                    className="btn btn-success col-4"
+                    onClick={() => this.setState(
+                      {
+                        modal: {
+                          callback: () => this.addFriend(),
+                          heading: 'Add Friend',
+                          text: 'Please select whom you want to add as a friend',
+                          userList: true,
+                          action: 'Add Friend',
+                        },
+                      },
+                      () => $('#myModal').modal('show')
+                    )}
+                  >
+                    add friend
+                  </button>
+                </div>
+                <div className="row">
+                  {
+                    this.state.user.friends.map((f, i) => (
+                      <div key={i} className="col-4 card">
+                        <img src={f.image && f.image.url} alt="" style={{ width: 'inherit' }}/>
+                        <h4>{`${f.name} ${f.surname}`}</h4>
+                      </div>
+                    ))
+                  }
+                </div>
               </div>
             </div>
             <div
@@ -145,15 +229,36 @@ class Profile extends React.Component {
                   {this.state.modal.text}
                 </div>
 
-                <input
-                  type="text"
-                  className="form-control col-10 offset-1"
-                  onChange={e => this.setState({ imageLink: e.nativeEvent.target.value })}
-                />
+                {
+                  this.state.modal.inputBox && <input
+                    type="text"
+                    className="form-control col-10 offset-1"
+                    onChange={e => this.setState({ imageLink: e.nativeEvent.target.value })}
+                  />
+                }
+                {
+                  <div className="col-10 offset-1">
+                    {
+                      this.state.modal.userList && this.state.users
+                        .filter(a => a.id !== this.state.user.id)
+                        .filter(a => this.state.user.friends.map(a => a.id).indexOf(a.id) < 0)
+                        .map(u => (
+                          <div
+                            className={`alert alert-${u.id === this.state.friendId ? 'info' : 'warning'} row`}
+                            key={u.id}
+                            onClick={() => this.setState({ friendId: u.id })}
+                          >
+                            <img src={u.image && u.image.url} alt="" style={{ width: 50, height: 50 }}/>
+                            <h4>{u.name}</h4>
+                          </div>
+                        ))
+                    }
+                  </div>
+                }
                 <br />
 
                 <div className="modal-footer">
-                  <button type="button" className="btn btn-success" data-dismiss="modal" onClick={this.state.modal.callback}>Upload</button>
+                  <button type="button" className="btn btn-success" data-dismiss="modal" onClick={this.state.modal.callback}>{this.state.modal.action}</button>
                 </div>
 
               </div>
